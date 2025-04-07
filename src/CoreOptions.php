67b4777c5f5303d1cbdc45c7640de7b141b4dcc2
John Shaffer <jdsha@pm.me>
2025-04-07 17:00:03 -0500

    Fix options set to '0' being replaced with default
    If the option in the DB was '0', it would be
    ignored and the default value would be used
    instead. Fix by only using the default if there
    is no string value present.

--- src/CoreOptions.php
+++ src/CoreOptions.php
@@ -411,7 +411,7 @@ VALUES (%s, %s, %s);";
 
         $option_value = $wpdb->get_var( $sql );
 
-        if ( ! $option_value || ! is_string( $option_value ) ) {
+        if ( ! is_string( $option_value ) ) {
             $option_value = (string) $opt_spec['default_value'];
         }
 
