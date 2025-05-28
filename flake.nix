3e10d88b56e9369318ab968d8adb112bf28df209
John Shaffer <jdsha@pm.me>
2025-05-28 11:36:29 -0500

    Provide currently supported PHP binaries in shell

--- flake.nix
+++ flake.nix
@@ -9,9 +9,19 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       with import nixpkgs { inherit system; };
-      with pkgs; {
+      with pkgs;
+      let
+        phpVersions = {
+          php81 = php81;
+          php82 = php82;
+          php83 = php83;
+          php84 = php84;
+        };
+        phpBins = lib.attrsets.mapAttrsToList (name: phpPkg:
+          pkgs.writeShellScriptBin name ''${phpPkg}/bin/php "$@"'') phpVersions;
+      in {
         devShells.default = mkShell {
-          buildInputs = [ php phpPackages.composer shellcheck zip ];
+          buildInputs = [ php phpPackages.composer shellcheck zip ] ++ phpBins;
         };
       });
 }
