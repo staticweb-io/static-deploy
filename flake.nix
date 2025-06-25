aa4107848bf564d84b47b7b274845cee38e642a6
John Shaffer <jdsha@pm.me>
2025-06-24 22:29:08 -0500

    Version 8.3.0

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       with pkgs;
       let
         name = "wp2static";
-        version = "8.2.0";
+        version = "8.3.0";
         composerDeps = php.buildComposerProject (finalAttrs: {
           pname = "${name}-composer-deps";
           version = version;
