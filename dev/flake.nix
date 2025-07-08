b4facb4bc503b03d5aa6d10d9b94dd5b31e01668
John Shaffer <jdsha@pm.me>
2025-07-08 13:46:59 -0500

    Fix unwriteable wp-config.php

--- dev/flake.nix
+++ dev/flake.nix
@@ -176,6 +176,7 @@
               command = ''
                 set -eu
                 mkdir -p ./data/wordpress1
+                chmod ug+w ./data/wordpress1/wp-config.php
                 cp "${wpConfig}" "./data/wordpress1/wp-config.php"
                 ${update-wordpress}/bin/update-wordpress ./data/wordpress1
               '';
