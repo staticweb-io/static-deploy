db51d77b00517d21b01610ec3c44136aee2268f3
John Shaffer <jdsha@pm.me>
2025-07-10 14:25:15 -0500

    Convert OptionsTest to PHPUnit

---
+++ tests/integration/OptionsTest.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace WP2Static;
+
+use PHPUnit\Framework\TestCase;
+
+final class OptionsTest extends TestCase {
+
+    use ITTrait;
+
+    public function testOptionFilters(): void
+    {
+        $plugin_dir = ITEnv::getWordPressDir() . '/wp-content/plugins/options-test';
+        exec( 'rm -rf ' . escapeshellarg( $plugin_dir ) );
+
+        $this->assertEquals( '0', $this->getOptionValue( 'processQueueImmediately' ) );
+
+        exec( 'mkdir -p ' . escapeshellarg( $plugin_dir ) );
+        file_put_contents( $plugin_dir . '/options-test.php', $this->optionsTestFilters() );
+        $this->wpCli( [ 'plugin', 'activate', 'options-test' ] );
+        $this->assertEquals( '1', $this->getOptionValue( 'processQueueImmediately' ) );
+
+        $this->wpCli( [ 'plugin', 'deactivate', 'options-test' ] );
+        exec( 'rm -rf ' . escapeshellarg( $plugin_dir ) );
+    }
+
+    private function optionsTestFilters(): string
+    {
+        return <<<PHP
+<?php
+
+/**
+ * Plugin Name: Options Test
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+function processQueueImmediately_filter ( $val ) {
+    return '1';
+}
+add_filter( 'wp2static_option_processQueueImmediately', 'processQueueImmediately_filter' );
+PHP;
+    }
+}
