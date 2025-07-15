3a9fd439afd05c4a1c07c9db65328c67f5468def
John Shaffer <jdsha@pm.me>
2025-07-15 13:02:07 -0500

    Remove outdated migrations
    Since we changed the table name, these column
    drops are not needed.

--- src/Options.php
+++ src/Options.php
@@ -41,22 +41,6 @@ class Options {
         require_once ABSPATH . 'wp-admin/includes/upgrade.php';
         dbDelta( $sql );
 
-        $columns = array_keys(
-            array_merge(
-                ...$wpdb->get_results(
-                    sprintf( 'SELECT * FROM %s LIMIT 1', $table_name ),
-                    ARRAY_A
-                )
-            )
-        );
-
-        if ( in_array( 'description', $columns ) ) {
-            $wpdb->query( "ALTER TABLE $table_name DROP COLUMN description" );
-        }
-        if ( in_array( 'label', $columns ) ) {
-            $wpdb->query( "ALTER TABLE $table_name DROP COLUMN label" );
-        }
-
         Controller::ensureIndex(
             $table_name,
             'name',
