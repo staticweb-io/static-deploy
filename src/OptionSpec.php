eca3aa46d71a1be744d7707102e3c6954b555699
John Shaffer <jdsha@pm.me>
2025-07-15 12:03:59 -0500

    Create OptionSpec class for option definitions

---
+++ src/OptionSpec.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * OptionSpec
+ *
+ * Specifies a configurable option for WP2Static
+ */
+
+declare(strict_types=1);
+
+namespace WP2Static;
+
+final class OptionSpec {
+
+    public readonly string $type;
+    public readonly string $name;
+    public readonly string $default_value;
+    public readonly string $label;
+    public readonly string $description;
+    public readonly ?string $default_blob_value;
+    public readonly string $filter_name;
+
+    public function __construct(
+        string $type,
+        string $name,
+        string $default_value,
+        string $label,
+        string $description,
+        ?string $default_blob_value = null,
+        ?string $filter_name = null
+    ) {
+        $this->type = $type;
+        $this->name = $name;
+        $this->default_value = $default_value;
+        $this->label = $label;
+        $this->description = $description;
+        $this->default_blob_value = $default_blob_value;
+        $this->filter_name = $filter_name ?? Controller::getHookName( "option_{$name}" );
+    }
+
+    /**
+     * @return array<string, ?string>
+     */
+    public function toArray(): array
+    {
+        return [
+            'type' => $this->type,
+            'name' => $this->name,
+            'default_value' => $this->default_value,
+            'label' => $this->label,
+            'description' => $this->description,
+            'default_blob_value' => $this->default_blob_value,
+            'filter_name' => $this->filter_name,
+        ];
+    }
+}
