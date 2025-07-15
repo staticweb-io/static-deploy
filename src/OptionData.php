96d73b81e618ee8aa07d105ede3726dd892d5be8
John Shaffer <jdsha@pm.me>
2025-07-15 12:24:50 -0500

    Add OptionData class
    This will replace the adhoc arrays and objects
    currently used for option data.

---
+++ src/OptionData.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * OptionData
+ *
+ * Specifies an option that has a configured or
+ * default value.
+ */
+
+declare(strict_types=1);
+
+namespace WP2Static;
+
+final class OptionData {
+
+    public readonly ?string $blob_value;
+    public readonly OptionSpec $option_spec;
+    public readonly ?string $unfiltered_blob_value;
+    public readonly string $unfiltered_value;
+    public readonly string $value;
+
+    public function __construct(
+        OptionSpec $option_spec,
+        ?string $blob_value,
+        string $unfiltered_value,
+    ) {
+        $this->blob_value = $blob_value;
+        $this->option_spec = $option_spec;
+        $this->unfiltered_blob_value = $blob_value;
+        $this->unfiltered_value = $unfiltered_value;
+
+        $value = $unfiltered_value;
+
+        if ( $this->option_spec->type === 'password' ) {
+            $value = Options::encrypt_decrypt( 'decrypt', $value );
+        }
+
+        $this->value = apply_filters(
+            $this->option_spec->filter_name,
+            $value
+        );
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(): array
+    {
+        return [
+            'blob_value' => $this->blob_value,
+            'option_spec' => $this->option_spec->toArray(),
+            'unfiltered_value' => $this->unfiltered_value,
+            'value' => $this->value,
+        ];
+    }
+}
