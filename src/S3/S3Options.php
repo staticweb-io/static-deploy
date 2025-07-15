eed5c8b8f9b0a80fa08ed65688e7313ec7c1dfb3
John Shaffer <jdsha@pm.me>
2025-07-15 14:53:38 -0500

    Create S3Options class with S3 option specs
    This will be used with WP2Static\Options to
    replace the existing S3 options code.

---
+++ src/S3/S3Options.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace WP2Static\S3;
+
+use WP2Static\OptionSpec;
+
+/*
+ * Options for the S3 deployer
+*/
+class S3Options {
+
+    /**
+     * @var array<string, OptionSpec>
+     */
+    private static $cached_option_specs;
+
+    public static function getName( string $slug ): string {
+        return 's3_' . $slug;
+    }
+
+    /**
+     * @return array<string, OptionSpec>
+     */
+    public static function optionSpecs(): array {
+        if ( isset( self::$cached_option_specs ) ) {
+            return self::$cached_option_specs;
+        }
+
+        $specs = [
+            // AWS credentials
+            new OptionSpec(
+                'string',
+                self::getName( 'awsAccessKeyId' ),
+                '',
+                'AWS Access Key ID',
+                'Access Key ID'
+            ),
+            new OptionSpec(
+                'string',
+                self::getName( 'awsProfile' ),
+                '',
+                'AWS Profile',
+                'Specifies which profile to use when credentials are ' .
+                'created from the AWS credentials file in your HOME directory.',
+            ),
+            new OptionSpec(
+                'string',
+                self::getName( 'awsRegion' ),
+                '',
+                'AWS Region',
+                'Region'
+            ),
+            new OptionSpec(
+                'string',
+                self::getName( 'awsSecretAccessKey' ),
+                '',
+                'AWS Secret Access Key',
+                'Secret Access Key'
+            ),
+
+            // S3 settings
+            new OptionSpec(
+                'string',
+                self::getName( 'bucketName' ),
+                '',
+                'S3 Bucket',
+                'Bucket name'
+            ),
+            new OptionSpec(
+                'string',
+                self::getName( 'bucketPrefix' ),
+                '',
+                'Path prefix in bucket',
+                'If set, uploads files to this path within the bucket.'
+            ),
+            new OptionSpec(
+                'integer',
+                self::getName( 'concurrency' ),
+                '4',
+                'Maximum number of files that will be uploaded at the same time',
+                ''
+            ),
+            new OptionSpec(
+                'string',
+                self::getName( 'headerCacheControl' ),
+                'public, max-age=900',
+                'Cache-Control header value',
+                '',
+                ''
+            ),
+            new OptionSpec(
+                'string',
+                self::getName( 'objectAcl' ),
+                'public-read',
+                'Object ACL',
+                '',
+                ''
+            ),
+
+            // CloudFront settings
+            new OptionSpec(
+                'string',
+                self::getName( 'distributionId' ),
+                '',
+                'CloudFront Distribution ID',
+                'If using CloudFront, set this to invalidate cache after deploying files.'
+            ),
+            new OptionSpec(
+                'integer',
+                self::getName( 'maxPathsToInvalidate' ),
+                '100',
+                'Max CloudFront paths to invalidate',
+                'Maximum number of paths to invalidate before triggering a full invalidation.',
+            ),
+        ];
+
+        $ret = [];
+        foreach ( $specs as $s ) {
+            $ret[ $s->name ] = $s;
+        }
+        self::$cached_option_specs = $ret;
+        return $ret;
+    }
+}
