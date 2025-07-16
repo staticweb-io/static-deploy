staticweb-io/static-deploy

commit 76dcd3f9cfd4c681f8934690755c57fad7c15ae5

Fix loose comparison operators

src/Utils.php
@@ -60,11 +60,11 @@ class Utils {
 
         $current_max_execution_time  = ini_get( 'max_execution_time' );
         $proposed_max_execution_time =
-            ( $current_max_execution_time == 30 ) ? 31 : 30;
+            ( $current_max_execution_time === 30 ) ? 31 : 30;
         set_time_limit( $proposed_max_execution_time );
         $current_max_execution_time = ini_get( 'max_execution_time' );
 
-        if ( $proposed_max_execution_time == $current_max_execution_time ) {
+        if ( $proposed_max_execution_time === $current_max_execution_time ) {
             set_time_limit( 0 );
         }
     }

src/CoreOptions.php
@@ -651,7 +651,7 @@ VALUES (%s, %s, %s);";
         $hex_key = (string) hex2bin( $key );
         $hex_iv = (string) hex2bin( $variate );
 
-        if ( $action == 'decrypt' ) {
+        if ( $action === 'decrypt' ) {
             return (string) openssl_decrypt(
                 (string) base64_decode( $string ),
                 $encrypt_method,

src/FileWriter.php
@@ -55,7 +55,7 @@ class FileWriter {
         }
 
         // set what the new path will be based on the given url
-        if ( $url_info['path'] != '/' ) {
+        if ( $url_info['path'] !== '/' ) {
             $path_info = pathinfo( $url_info['path'] );
         } else {
             $path_info = pathinfo( 'index.html' );
@@ -83,16 +83,16 @@ class FileWriter {
 
         if ( isset( $path_info['extension'] ) ) {
             $file_extension = $path_info['extension'];
-        } elseif ( $this->file_type == 'html' ) {
+        } elseif ( $this->file_type === 'html' ) {
             $file_extension = 'html';
-        } elseif ( $this->file_type == 'xml' ) {
+        } elseif ( $this->file_type === 'xml' ) {
             $file_extension = 'html';
         }
 
         $filename = '';
 
         // set path for homepage to index.html, else build filename
-        if ( $url_info['path'] == '/' ) {
+        if ( $url_info['path'] === '/' ) {
             // TODO: isolate and fix the cause requiring this trim:
             $filename = rtrim( $file_dir, '.' ) . 'index.html';
         } else {

src/ProcessedSite.php
@@ -93,7 +93,7 @@ class ProcessedSite {
              */
 
             $base_name = basename( $filename );
-            if ( $base_name != '.' && $base_name != '..' ) {
+            if ( $base_name !== '.' && $base_name !== '..' ) {
                 $real_filepath = realpath( $filename );
 
                 if ( is_string( $real_filepath ) ) {

src/SitemapParser.php
@@ -352,7 +352,6 @@ class SitemapParser {
         }
         return $array;
     }
-
     /**
      * Generate the \SimpleXMLElement object if the XML is valid
      *
@@ -419,11 +418,11 @@ class SitemapParser {
                 substr(
                     (string) $path,
                     -strlen( self::XML_EXTENSION ) - 1
-                ) == '.' . self::XML_EXTENSION ||
+                ) === '.' . self::XML_EXTENSION ||
                 substr(
                     (string) $path,
                     -strlen( self::XML_EXTENSION_COMPRESSED ) - 1
-                ) == '.' . self::XML_EXTENSION_COMPRESSED
+                ) === '.' . self::XML_EXTENSION_COMPRESSED
             );
     }
 

src/StaticSite.php
@@ -80,7 +80,7 @@ class StaticSite {
              * @var string $filename
              */
             $base_name = basename( $filename );
-            if ( $base_name != '.' && $base_name != '..' ) {
+            if ( $base_name !== '.' && $base_name !== '..' ) {
                 $real_filepath = realpath( $filename );
 
                 if ( is_string( $real_filepath ) ) {

src/URLHelper.php
@@ -7,7 +7,7 @@ use Exception;
 class URLHelper {
     public static function isSecure(): bool {
         return ( ! empty( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] !== 'off' ) ||
-            $_SERVER['SERVER_PORT'] == 443;
+            $_SERVER['SERVER_PORT'] === 443;
     }
 
     /*
@@ -103,7 +103,7 @@ class URLHelper {
     }
 
     public static function isMailto( string $url ): bool {
-        if ( substr( $url, 0, 7 ) == 'mailto:' ) {
+        if ( substr( $url, 0, 7 ) === 'mailto:' ) {
             return true;
         }
 

views/diagnostics-page.php
@@ -22,11 +22,11 @@
             <tr>
                 <td>PHP max_execution_time</td>
                 <td>
-                    <?php echo $view['maxExecutionTime'] == 0 ? 'Unlimited' : $view['maxExecutionTime'] . ' secs'; ?>
+                    <?php echo $view['maxExecutionTime'] === 0 ? 'Unlimited' : $view['maxExecutionTime'] . ' secs'; ?>
 
                     <span
-                        class="dashicons <?php echo $view['maxExecutionTime'] == 0 ? 'dashicons-yes' : 'dashicons-no'; ?>"
-                        style="color: <?php echo $view['maxExecutionTime'] == 0 ? 'green' : 'red'; ?>;"
+                        class="dashicons <?php echo $view['maxExecutionTime'] === 0 ? 'dashicons-yes' : 'dashicons-no'; ?>"
+                        style="color: <?php echo $view['maxExecutionTime'] === 0 ? 'green' : 'red'; ?>;"
                     ></span>
                 </td>
                 <td>Generating a static site can involve long-running processes. Set your PHP max_execution_time setting to unlimited or find a better webhost if you're prevented from doing so.</td>

commit 525fd1213182f2903671e64c184be97ec298367a

Fix max_execution_time being set to 30 and not 0
This was a regression introduced in
76dcd3f9cfd4c681f8934690755c57fad7c15ae5.
Changing the loose == operators to strict ===
caused behavior to change. max_execution_time got
set to 30 instead of 0 as intended.

Fix by calling intval() so that the ===
comparisons work.

src/Utils.php
@@ -58,11 +58,11 @@ class Utils {
             return;
         }
 
-        $current_max_execution_time  = ini_get( 'max_execution_time' );
+        $current_max_execution_time  = intval( ini_get( 'max_execution_time' ) );
         $proposed_max_execution_time =
             ( $current_max_execution_time === 30 ) ? 31 : 30;
         set_time_limit( $proposed_max_execution_time );
-        $current_max_execution_time = ini_get( 'max_execution_time' );
+        $current_max_execution_time = intval( ini_get( 'max_execution_time' ) );
 
         if ( $proposed_max_execution_time === $current_max_execution_time ) {
             set_time_limit( 0 );