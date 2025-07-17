39625a82e40a1973072283ad23eecadd428ed595
John Shaffer <jdsha@pm.me>
2025-07-17 12:57:48 -0500

    Add admin hooks to OptionsControllerTrait

--- src/OptionsControllerTrait.php
+++ src/OptionsControllerTrait.php
@@ -7,6 +7,54 @@ namespace StaticDeploy;
  * and an options page.
  */
 trait OptionsControllerTrait {
+    public static function registerHooks(): void {
+        add_action(
+            'admin_post_' . self::getAdminAction(),
+            [ self::class, 'saveFromAdmin' ],
+            15,
+            1
+        );
+
+        add_action(
+            'admin_menu',
+            [ self::class, 'addOptionsPage' ],
+            15,
+            1
+        );
+
+        add_filter(
+            Controller::getHookName( 'add_menu_items' ),
+            [ self::class, 'addSubmenuPage' ]
+        );
+    }
+
+    public static function addOptionsPage(): void {
+        $title = self::getPageData()['title'];
+        add_submenu_page(
+            '',
+            $title,
+            $title,
+            'manage_options',
+            self::getOptionsPageSlug(),
+            [ self::class, 'renderPage' ]
+        );
+    }
+
+    /**
+     * Add submenu
+     *
+     * @param mixed[] $submenu_pages array of submenu pages
+     * @return mixed[] array of submenu pages
+     */
+    public static function addSubmenuPage( array $submenu_pages ): array {
+        $submenu_pages[ self::getOptionsPageSlug() ] = [
+            self::class,
+            'renderPage',
+        ];
+
+        return $submenu_pages;
+    }
+
     public static function renderPage(): void {
         $option_specs = self::getSpecs();
         Options::seedOptions( $option_specs );
@@ -21,5 +69,15 @@ trait OptionsControllerTrait {
         ];
 
         require_once __DIR__ . '/../views/render-options-page.php';
+    }
+
+    public static function saveFromAdmin(): void {
+        check_admin_referer( self::getAdminAction() );
+
+        Options::saveFromAdmin( self::getSpecs() );
+
+        $url = 'admin.php?page=' . self::getOptionsPageSlug();
+        wp_safe_redirect( admin_url( $url ) );
+        exit;
     }
 }
