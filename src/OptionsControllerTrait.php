eba10c49c137fd879c2f89f1fbf9099b277b1188
John Shaffer <jdsha@pm.me>
2025-07-17 12:44:58 -0500

    Add OptionsControllerTrait with page renderer
    Add a trait for controllers that manage options
    and an options page.
    
    Start with a renderPage() function that can
    build an options page.

---
+++ src/OptionsControllerTrait.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace StaticDeploy;
+
+/*
+ * Trait for controllers that manage a set of options
+ * and an options page.
+ */
+trait OptionsControllerTrait {
+    public static function renderPage(): void {
+        $option_specs = self::getSpecs();
+        Options::seedOptions( $option_specs );
+
+        $page = self::getPageData();
+
+        $view = [
+            'nonce_action' => self::getAdminAction(),
+            'options' => Options::getAll( $option_specs ),
+            'sections' => $page['sections'],
+            'title' => $page['title'],
+        ];
+
+        require_once __DIR__ . '/../views/render-options-page.php';    }
+}
