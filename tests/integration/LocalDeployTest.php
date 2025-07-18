796aae8043f67713167320c18ca156ac8f8ae443
John Shaffer <jdsha@pm.me>
2025-07-18 14:09:39 -0500

    Add test for deploy command with LocalDeployer

--- tests/integration/LocalDeployTest.php
+++ tests/integration/LocalDeployTest.php
@@ -8,8 +8,22 @@ final class LocalDeployTest extends TestCase {
 
     use ITTrait;
 
-    public function testLocalDirectDeploy(): void
-    {
+    public function testLocalDeploy(): void {
+        $this->pluginCli( [ 'addons', 'enable', 'static-deploy-addon-local' ] );
+        $this->pluginCli( [ 'options', 'set', 'local_dirPath', '../localdeploy' ] );
+        $this->pluginCli( [ 'full_workflow' ] );
+
+        $content = $this->getLocalDeployFileContents( 'index.html' );
+        $this->assertStringContainsString( 'Welcome to WordPress', $content );
+
+        $content = $this->getLocalDeployFileContents( 'robots.txt' );
+        $this->assertStringContainsString(
+            'Sitemap: https://example.com/wp-sitemap.xml',
+            $content
+        );
+    }
+
+    public function testLocalDirectDeploy(): void {
         $this->pluginCli( [ 'addons', 'enable', 'static-deploy-addon-local' ] );
         $this->pluginCli( [ 'options', 'set', 'local_dirPath', '../localdeploy' ] );
         $this->pluginCli( [ 'direct_deploy' ] );
