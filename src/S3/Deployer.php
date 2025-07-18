c5b7481b4e487df6e2f6b316d24bca9b1daa4fcd
John Shaffer <jdsha@pm.me>
2025-07-18 13:57:44 -0500

    Fix wrong reference to S3 deployer namespace

--- src/S3/Deployer.php
+++ src/S3/Deployer.php
@@ -193,7 +193,7 @@ class Deployer {
 
                 $is_cached = DeployCache::fileisCached(
                     $cache_key,
-                    $this->namespace,
+                    self::DEFAULT_NAMESPACE,
                     $hash,
                 );
 
@@ -231,7 +231,7 @@ class Deployer {
                     $item = $items_by_iter_key[ $iter_key ];
                     DeployCache::addFile(
                         $item['cache_key'],
-                        $this->namespace,
+                        self::DEFAULT_NAMESPACE,
                         $item['hash']
                     );
                     $this->addCfPath( $item['cache_key'] );
