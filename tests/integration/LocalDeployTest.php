c84b7b3d44896fc4f2d33682fdbd065ca4982bd1
John Shaffer <jdsha@pm.me>
2025-07-18 13:39:16 -0500

    Add tests for local deployer direct_deploy

---
+++ tests/integration/LocalDeployTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace StaticDeploy;
+
+use PHPUnit\Framework\TestCase;
+
+final class LocalDeployTest extends TestCase {
+
+    use ITTrait;
+
+    public function testLocalDirectDeploy(): void
+    {
+        $this->pluginCli( [ 'addons', 'enable', 'static-deploy-addon-local' ] );
+        $this->pluginCli( [ 'options', 'set', 'local_dirPath', '../localdeploy' ] );
+        $this->pluginCli( [ 'direct_deploy' ] );
+
+        $content = $this->getLocalDeployFileContents( 'index.html' );
+        $this->assertStringContainsString( 'Welcome to WordPress', $content );
+
+        $content = $this->getLocalDeployFileContents( 'robots.txt' );
+        $this->assertStringContainsString(
+            'Sitemap: https://example.com/wp-sitemap.xml',
+            $content
+        );
+    }
+}
