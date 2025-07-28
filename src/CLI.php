071c96cda8c39de62cd866ad1aa6f0e76b45fc06
John Shaffer <jdsha@pm.me>
2025-07-28 11:37:12 -0500

    Move parse fn to CLI\Args
    I'm planning on moving code out of CLI.php and
    files in CLI/ for each subcommand. This will be
    a convenient spot for argument functions.

--- src/CLI.php
+++ src/CLI.php
@@ -2,6 +2,7 @@
 
 namespace StaticDeploy;
 
+use StaticDeploy\CLI\Args;
 use WP_CLI;
 
 /**
@@ -12,58 +13,6 @@ class CLI {
      * @var array<string>
      */
     private $assoc_args = null;
-
-    public static function parse(
-        array $args,
-        ?array $assoc_args = null,
-        ?array $opts = null,
-        ?array $assoc_opts = null,
-    ): array {
-        if ( STATIC_DEPLOY_DEBUG ) {
-            WsLog::d( 'Parsing CLI args: ' . json_encode( $args ) );
-            WsLog::d( 'CLI options: ' . json_encode( $opts ) );
-            WsLog::d( 'Parsing CLI assoc args: ' . json_encode( $assoc_args ) );
-            WsLog::d( 'CLI assoc options: ' . json_encode( $assoc_opts ) );
-        }
-
-        $cfg = [];
-
-        $n_args = count( $args );
-        $opts_keys = array_keys( $opts ?? [] );
-        for ( $i = 0; $i < $n_args; $i++ ) {
-            $k = $opts_keys[ $i ] ?? null;
-            if ( $k === null ) {
-                WP_CLI::error( 'Too many arguments' );
-            }
-            $cfg[ $k ] = $args[ $i ];
-        }
-
-        foreach ( $assoc_args as $k => $v ) {
-            if ( ! array_key_exists( $k, $assoc_opts ) ) {
-                WP_CLI::error( 'Unrecognized option: --' . $k );
-            }
-
-            $cfg[ $k ] = $v;
-        }
-
-        foreach ( $assoc_opts as $k => $v ) {
-            if ( ! array_key_exists( $k, $cfg ) ) {
-                if ( $v && array_key_exists( 'default', $v ) ) {
-                    $cfg[ $k ] = $v['default'];
-                } elseif ( $v['required'] ?? false ) {
-                    WP_CLI::error( 'Missing required option: --' . $k );
-                } else {
-                    $cfg[ $k ] = null;
-                }
-            }
-        }
-
-        if ( STATIC_DEPLOY_DEBUG ) {
-            WsLog::d( 'Parsed CLI args: ' . json_encode( $cfg ) );
-        }
-
-        return $cfg;
-    }
 
     /**
      * Display system information and health check
@@ -202,7 +151,7 @@ class CLI {
             'detect' => null,
             'path-hash-prefix' => null,
         ];
-        $cfg = self::parse( $args, $assoc_args, $opts, $assoc_opts );
+        $cfg = Args::parse( $args, $assoc_args, $opts, $assoc_opts );
 
         $path_hash_prefix = strval( $cfg['path-hash-prefix'] );
 
@@ -381,7 +330,7 @@ class CLI {
         $assoc_opts = [
             'path-hash-prefix' => null,
         ];
-        $cfg = self::parse( $args, $assoc_args, $opts, $assoc_opts );
+        $cfg = Args::parse( $args, $assoc_args, $opts, $assoc_opts );
 
         $path_hash_prefix = strval( $cfg['path-hash-prefix'] );
 
---
+++ src/CLI/Args.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+
+namespace StaticDeploy\CLI;
+
+use WP_CLI;
+
+class Args {
+    public static function parse(
+        array $args,
+        ?array $assoc_args = null,
+        ?array $opts = null,
+        ?array $assoc_opts = null,
+    ): array {
+        if ( STATIC_DEPLOY_DEBUG ) {
+            WsLog::d( 'Parsing CLI args: ' . json_encode( $args ) );
+            WsLog::d( 'CLI options: ' . json_encode( $opts ) );
+            WsLog::d( 'Parsing CLI assoc args: ' . json_encode( $assoc_args ) );
+            WsLog::d( 'CLI assoc options: ' . json_encode( $assoc_opts ) );
+        }
+
+        $cfg = [];
+
+        $n_args = count( $args );
+        $opts_keys = array_keys( $opts ?? [] );
+        for ( $i = 0; $i < $n_args; $i++ ) {
+            $k = $opts_keys[ $i ] ?? null;
+            if ( $k === null ) {
+                WP_CLI::error( 'Too many arguments' );
+            }
+            $cfg[ $k ] = $args[ $i ];
+        }
+
+        foreach ( $assoc_args as $k => $v ) {
+            if ( ! array_key_exists( $k, $assoc_opts ) ) {
+                WP_CLI::error( 'Unrecognized option: --' . $k );
+            }
+
+            $cfg[ $k ] = $v;
+        }
+
+        foreach ( $assoc_opts as $k => $v ) {
+            if ( ! array_key_exists( $k, $cfg ) ) {
+                if ( $v && array_key_exists( 'default', $v ) ) {
+                    $cfg[ $k ] = $v['default'];
+                } elseif ( $v['required'] ?? false ) {
+                    WP_CLI::error( 'Missing required option: --' . $k );
+                } else {
+                    $cfg[ $k ] = null;
+                }
+            }
+        }
+
+        if ( STATIC_DEPLOY_DEBUG ) {
+            WsLog::d( 'Parsed CLI args: ' . json_encode( $cfg ) );
+        }
+
+        return $cfg;
+    }
+}
