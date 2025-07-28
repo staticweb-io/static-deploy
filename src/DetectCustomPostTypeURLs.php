7ef6c35b2b1d2fc4eb04bd79ab7eb790ab272f2a
John Shaffer <jdsha@pm.me>
2025-07-28 15:29:35 -0500

    Don't pick up wp_navigation URLs in detection

--- src/DetectCustomPostTypeURLs.php
+++ src/DetectCustomPostTypeURLs.php
@@ -20,7 +20,7 @@ class DetectCustomPostTypeURLs {
             "SELECT ID
             FROM {$wpdb->posts}
             WHERE post_status = 'publish'
-            AND post_type NOT IN ('revision','nav_menu_item')"
+            AND post_type NOT IN ('nav_menu_item','revision','wp_navigation')"
         );
 
         foreach ( $post_ids as $post_id ) {
