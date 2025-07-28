6d7a96b3c5cc90c67dff0d700420931b39e19307
John Shaffer <jdsha@pm.me>
2025-07-28 13:54:14 -0500

    Implement running raw memcached commands

--- src/Memcached.php
+++ src/Memcached.php
@@ -24,4 +24,96 @@ class Memcached {
         }
         return null;
     }
+
+    /**
+     * Returns a server at random, respecting weight.
+     *
+     * Returns an array of the form:
+     * [
+     *     'host' => '127.0.0.1',
+     *     'port' => 11211,
+     *     'weight' => 20,
+     * ]
+     */
+    public static function getRandomServer(
+        \Memcached $mc,
+    ): array {
+        $servers = $mc->getServerList();
+        $total_weight = 0;
+
+        foreach ( $servers as $s ) {
+            $total_weight += $s['weight'] ?? 1;
+        }
+
+        $rand = rand( 1, $total_weight );
+        $current = 0;
+
+        foreach ( $servers as $s ) {
+            $weight = $s['weight'] ?? 1;
+            $current += $weight;
+            if ( $rand <= $current ) {
+                return $s;
+            }
+        }
+    }
+
+    /**
+     * Returns a socket to a server in the memcached pool
+     *
+     * @param int &$error_code Error code passed to fsockopen
+     * @param string &$error_message Error message passed to fsockopen
+     */
+    public static function getSocket(
+        \Memcached $mc,
+        int &$error_code = null,
+        string &$error_message = null,
+    ) {
+        $server = self::getRandomServer( $mc );
+        $host = $server['host'];
+        $port = $server['port'];
+
+        $sock = fsockopen(
+            $host,
+            $port,
+            $error_code,
+            $error_message,
+            1.0
+        );
+
+        return $sock;
+    }
+
+    /**
+     * Execute a command over the memcached text protocol.
+     * Returns an \Iterator of the result lines.
+     */
+    public static function doCommand(
+        \Memcached $mc,
+        string $command,
+    ): \Iterator {
+        $error_code = null;
+        $error_message = null;
+        $sock = self::getSocket( $mc, $error_code, $error_message );
+        if ( ! $sock ) {
+            throw WsLog::ex(
+                'Failed to connect to Memcached at ' .
+                $host . ':' . $port . ': ' . $error_code . ' ' . $error_message
+            );
+        }
+
+        fwrite( $sock, $command . "\r\n" );
+
+        $output = [];
+        $count = 0;
+        while ( ! feof( $sock ) ) {
+            $line = fgets( $sock );
+            if ( $line === false || rtrim( $line ) === 'END' ) {
+                return;
+            }
+            yield $line;
+        }
+
+        fclose( $sock );
+    }
+
 }
