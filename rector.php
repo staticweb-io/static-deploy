39f54dd28e9758cdb986d663ed566e856bcc8800
John Shaffer <jdsha@pm.me>
2025-08-11 11:46:35 -0500

    Tell rector not to remove side-effecting foreach
    The crawler currently has a loop that only
    causes side-effects:
    
    foreach ( $crawled as $_ ) {
        // Intentionally empty to consume the iterator
        continue;
    }
    
    This is a valid pattern. Configure rector not to
    treat this as dead code.

--- rector.php
+++ rector.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\For_\RemoveDeadContinueRector;
 use Rector\Set\ValueObject\SetList;
 
 return RectorConfig::configure()
@@ -12,4 +13,10 @@ return RectorConfig::configure()
             __DIR__ . '/views',
         ]
     )
-    ->withSets( [ SetList::DEAD_CODE ] );
+    ->withSets( [ SetList::DEAD_CODE ] )
+    ->withSkip(
+        [
+            // Allow explicit loops to consume iterators for side-effects
+            RemoveDeadContinueRector::class,
+        ]
+    );
